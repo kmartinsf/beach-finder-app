da9b644af2583b6901de186ca772286a16c85552
Kaiena <kaienarmartins@gmail.com>
2025-06-23 11:48:05 -0300

    chore: update GitHub Actions workflow and Jest configuration for Firebase integration

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
-  preset: "jest-expo",
-  transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)",
-  ],
+  preset: 'react-native',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@firebase/(.*)$': '<rootDir>/node_modules/@firebase/$1',
   },
-  setupFiles: ["<rootDir>/jest.setup.js"],
-  setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
-  testEnvironment: "jsdom",
+  transformIgnorePatterns: [
+    'node_modules/(?!(@react-native|react-native|react-native-safe-area-context|@firebase)/)',
+  ],
+  setupFiles: [
+    './jest.setup.js'
+  ],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
 };
