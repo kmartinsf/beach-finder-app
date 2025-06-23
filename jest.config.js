8b13d5609f0a4584432670b212f00da10f9feb85
Kaiena <kaienarmartins@gmail.com>
2025-06-23 16:01:39 -0300

    fix: update import path for firebase utility module

--- jest.config.js
+++ jest.config.js
@@ -4,14 +4,16 @@ module.exports = {
   preset: "jest-expo",
   setupFiles: ["./jest.setup.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!(expo|@expo|expo-router|expo-modules-core|expo-asset|expo-constants|expo-linking|react-native|@react-native|react-navigation|@react-navigation)/)"
+    "node_modules/(?!(expo|@expo|expo-router|expo-modules-core|expo-asset|expo-constants|expo-linking|react-native|@react-native|react-navigation|@react-navigation)/)",
   ],
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
     "^@/utils/firebase$": "<rootDir>/tests/__mocks__/utils/firebase.ts",
+    "^@/utils/internal/firebase$":
+      "<rootDir>/tests/__mocks__/utils/firebase.ts",
   },
   moduleDirectories: ["node_modules", path.resolve(__dirname)],
   transform: {
-    "^.+\\.[jt]sx?$": "babel-jest"
-  }
+    "^.+\\.[jt]sx?$": "babel-jest",
+  },
 };
