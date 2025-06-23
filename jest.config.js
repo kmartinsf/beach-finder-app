7d61248037075d0726dbb5307585eb3fa9113d10
Kaiena <kaienarmartins@gmail.com>
2025-06-23 15:01:20 -0300

    chore: format jest.config.js for consistency and readability

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,18 @@
+const path = require("path");
+
 module.exports = {
-  preset: 'jest-expo',
-  setupFiles: ['./jest.setup.js'],
+  preset: "jest-expo",
+  setupFiles: ["./jest.setup.js"],
   transformIgnorePatterns: [
-    'node_modules/(?!(expo|@expo|expo-router|expo-modules-core|expo-asset|expo-constants|expo-linking|react-native|@react-native|react-navigation|@react-navigation)/)',
+    "node_modules/(?!(expo|@expo|expo-router|expo-modules-core|expo-asset|expo-constants|expo-linking|react-native|@react-native|react-navigation|@react-navigation)/)",
   ],
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    "^@/(.*)$": "<rootDir>/$1",
   },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
+    "^.+\\.[jt]sx?$": "babel-jest",
   },
+  rootDir: ".",
+  moduleDirectories: ["node_modules", path.join(__dirname, ".")],
 };
-
