54fb3fd9c7153cda83987d028801201b4d775a71
Kaiena <kaienarmartins@gmail.com>
2025-06-23 15:05:47 -0300

    chore: clean up jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,13 @@ module.exports = {
   preset: "jest-expo",
   setupFiles: ["./jest.setup.js"],
   transformIgnorePatterns: [
-    "node_modules/(?!(expo|@expo|expo-router|expo-modules-core|expo-asset|expo-constants|expo-linking|react-native|@react-native|react-navigation|@react-navigation)/)",
+    "node_modules/(?!(expo|@expo|expo-router|expo-modules-core|expo-asset|expo-constants|expo-linking|react-native|@react-native|react-navigation|@react-navigation)/)"
   ],
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/$1",
+    "^@/(.*)$": "<rootDir>/$1"
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  transform: {
-    "^.+\\.[jt]sx?$": "babel-jest",
-  },
-  rootDir: ".",
   moduleDirectories: ["node_modules", path.join(__dirname, ".")],
+  transform: {
+    "^.+\\.[jt]sx?$": "babel-jest"
+  }
 };
