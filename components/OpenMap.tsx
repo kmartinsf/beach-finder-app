7e886c996c792126b216f0a1c5910c28c7c582e1
Kaiena <kaienarmartins@gmail.com>
2025-07-10 10:37:59 -0300

    fix: disable new architecture and update dependencies
    - Set "newArchEnabled" to false in app.json.
    - Added additional properties for splash screen and adaptive icon in app.json.
    - Updated OpenMap component to include dialog titles and messages.

--- components/OpenMap.tsx
+++ components/OpenMap.tsx
@@ -3,6 +3,8 @@ import { showLocation } from "react-native-map-link";
 const OpenMap = (beach: string) => {
   showLocation({
     address: `${beach}, Florianópolis, Santa Catarina, Brasil`,
+    dialogTitle: "Abrir Localização",
+    dialogMessage: "Selecione o aplicativo de GPS para traçar a rota ou visualizar este ponto",
     cancelText: "Cancelar",
   });
 };
